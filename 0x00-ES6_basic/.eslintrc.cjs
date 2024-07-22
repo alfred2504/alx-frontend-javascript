e35b699e8c422112dfa339796f9f22f16753ebce
alfred2504 <inforamaiv@gmail.com>
2024-07-22 12:16:53 +0000

    README.md

---
+++ 0x00-ES6_basic/.eslintrc.cjs
@@ -0,0 +1,25 @@
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
